cd2153ceee31a588038e6f9b4511809e98eff230
Ou Guozheng <abc@abc.com>
2021-06-11 16:56:17 +0800

    Added where clause

--- infra-jpa/src/main/kotlin/com/guozheng/kotlin/infra/jpa/entities/PersonEntity.kt
+++ infra-jpa/src/main/kotlin/com/guozheng/kotlin/infra/jpa/entities/PersonEntity.kt
@@ -3,6 +3,7 @@ package com.guozheng.kotlin.infra.jpa.entities
 import lombok.Builder
 import lombok.Data
 import lombok.NoArgsConstructor
+import org.hibernate.annotations.Where
 import org.hibernate.envers.Audited
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
@@ -14,6 +15,8 @@ import javax.persistence.*
 @NoArgsConstructor
 @Audited
 @EntityListeners(AuditingEntityListener::class)
+@Where(clause = "DELETED_AT IS NULL")
+@Table(name = "persons")
 class PersonEntity(
     @Column var name: String?,
     createdAt: LocalDateTime?,
