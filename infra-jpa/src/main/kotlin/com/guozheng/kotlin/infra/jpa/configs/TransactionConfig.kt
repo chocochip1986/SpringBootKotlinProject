208a1d8ebb26086d6248cbc54998632e5d5fbe63
Ou Guozheng <abc@abc.com>
2021-06-11 16:59:38 +0800

    Addeed transaction

---
+++ infra-jpa/src/main/kotlin/com/guozheng/kotlin/infra/jpa/configs/TransactionConfig.kt
@@ -0,0 +1,9 @@
+package com.guozheng.kotlin.infra.jpa.configs
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.transaction.annotation.EnableTransactionManagement
+
+@Configuration
+@EnableTransactionManagement
+class TransactionConfig {
+}
